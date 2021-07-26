4a9885c4f49e71029d9a6e2804b1727af34c2fd7
Cory Goates <cory.goates@aggiemail.usu.edu>
2021-07-26 14:29:26 -0600

    Implemented pressure correction equations.

--- ns.f95
+++ ns.f95
@@ -7,13 +7,37 @@ program navier_stokes
     character(100) :: input_file
     type(ns_case_t) :: case
 
+    ! Print welcome message
+    write(*,*) ""
+    write(*,*) "-------------------------------------------------"
+    write(*,*) "|                                               |"
+    write(*,*) "|  Welcome to the Flow81 Navier-Stokes Solver!  |"
+    write(*,*) "|                                               |"
+    write(*,*) "|      Created by Cory Goates, (c) 2021         |"
+    write(*,*) "|                                               |"
+    write(*,*) "-------------------------------------------------"
+    write(*,*) ""
+
     ! Load input
     call getarg(1, input_file)
+    write(*,*) "Got input file: ", input_file
+    write(*,*) "Loading input..."
     call ns_case_load_input(case, input_file)
 
     ! Run case
+    write(*,*) ""
+    write(*,*) "Running case..."
     call ns_case_run_simple(case)
 
     ! Output
+
+    ! Print quit message
+    write(*,*) ""
+    write(*,*) "-------------------------------------------------"
+    write(*,*) "|                                               |"
+    write(*,*) "|         Flow81 finished successfully          |"
+    write(*,*) "|                                               |"
+    write(*,*) "-------------------------------------------------"
+    write(*,*) ""
 
 end program navier_stokes
--- ns_case.f95
+++ ns_case.f95
@@ -12,9 +12,10 @@ module ns_case_m
         real(kind=8) :: dx, dy
         integer :: nx, ny, outer_iterations
         type(bc_t) :: Uw_bc, Vw_bc, Us_bc, Vs_bc, Ue_bc, Ve_bc, Un_bc, Vn_bc
-        real(kind=8),allocatable,dimension(:,:) :: U, U_old, V, V_old, P ! Values
+        real(kind=8),allocatable,dimension(:,:) :: U, U_old, V, V_old, P, P_prime ! Values
         real(kind=8),allocatable,dimension(:,:) :: AuP, AuW, AuS, AuE, AuN ! x-mom coefficients
         real(kind=8),allocatable,dimension(:,:) :: AvP, AvW, AvS, AvE, AvN ! y-mom coefficients
+        real(kind=8),allocatable,dimension(:,:) :: AP, AW, AS, AE, AN, S ! pressure coefficients
         real(kind=8),allocatable,dimension(:) :: x_cp, y_cp
 
     end type ns_case_t
@@ -31,17 +32,32 @@ subroutine ns_case_allocate(t)
 
     ! Allocate arrays
     allocate(t%P(1:t%nx,1:t%ny))
+    allocate(t%P_prime(1:t%nx,1:t%ny))
     allocate(t%U(1:t%nx+1,0:t%ny+1))
     allocate(t%V(0:t%nx+1,1:t%ny+1))
     allocate(t%U_old(1:t%nx+1,0:t%ny+1))
     allocate(t%V_old(0:t%nx+1,1:t%ny+1))
     allocate(t%x_cp(t%nx))
     allocate(t%y_cp(t%ny))
+
     allocate(t%AuW(2:t%nx,1:t%ny))
     allocate(t%AuS(2:t%nx,1:t%ny))
     allocate(t%AuE(2:t%nx,1:t%ny))
     allocate(t%AuN(2:t%nx,1:t%ny))
     allocate(t%AuP(2:t%nx,1:t%ny))
+
+    allocate(t%AvW(1:t%nx,2:t%ny))
+    allocate(t%AvS(1:t%nx,2:t%ny))
+    allocate(t%AvE(1:t%nx,2:t%ny))
+    allocate(t%AvN(1:t%nx,2:t%ny))
+    allocate(t%AvP(1:t%nx,2:t%ny))
+
+    allocate(t%AW(1:t%nx,1:t%ny))
+    allocate(t%AS(1:t%nx,1:t%ny))
+    allocate(t%AE(1:t%nx,1:t%ny))
+    allocate(t%AN(1:t%nx,1:t%ny))
+    allocate(t%AP(1:t%nx,1:t%ny))
+    allocate(t%S(1:t%nx,1:t%ny))
 
     allocated = 1
 
@@ -53,12 +69,32 @@ subroutine ns_case_deallocate(t)
 
     ! Clean up memory
     deallocate(t%P)
+    deallocate(t%P_prime)
     deallocate(t%U)
     deallocate(t%V)
     deallocate(t%U_old)
     deallocate(t%V_old)
     deallocate(t%x_cp)
     deallocate(t%y_cp)
+
+    deallocate(t%AuW)
+    deallocate(t%AuS)
+    deallocate(t%AuE)
+    deallocate(t%AuN)
+    deallocate(t%AuP)
+
+    deallocate(t%AvW)
+    deallocate(t%AvS)
+    deallocate(t%AvE)
+    deallocate(t%AvN)
+    deallocate(t%AvP)
+
+    deallocate(t%AW)
+    deallocate(t%AS)
+    deallocate(t%AE)
+    deallocate(t%AN)
+    deallocate(t%AP)
+    deallocate(t%S)
 
     allocated = 0
 
@@ -130,6 +166,7 @@ subroutine ns_case_initialize(t)
 
     ! Guess values for U, V, and P
     t%P = 0.0
+    t%P_prime = 0.0
     t%U = 0.0
     t%V = 0.0
 
@@ -205,10 +242,10 @@ subroutine ns_case_run_simple(t)
         call ns_case_x_mom(t)
 
         ! Handle y-momentum
+        call ns_case_y_mom(t)
 
-        ! Solve pressure correction
-
-        ! Update pressure
+        ! Pressure correction
+        call ns_case_pressure_corr(t)
 
         ! Correct velocities based on pressure corrections
 
@@ -230,22 +267,29 @@ subroutine ns_case_x_mom(t)
         do j=1,t%ny
 
             ! Mass flux terms
-            mw = 0.5*t%rho*(t%U(i-1,j)+t%U(i,j))
-            ms = 0.5*t%rho*(t%U(i,j-1)+t%U(i,j))
-            me = 0.5*t%rho*(t%U(i,j)+t%U(i+1,j))
-            me = 0.5*t%rho*(t%U(i,j)+t%U(i,j+1))
+            mw = 0.5*t%rho*(t%U_old(i-1,j)+t%U_old(i,j))
+            ms = 0.5*t%rho*(t%V_old(i,j-1)+t%V_old(i,j))
+            me = 0.5*t%rho*(t%U_old(i,j)+t%U_old(i+1,j))
+            me = 0.5*t%rho*(t%V_old(i,j)+t%V_old(i,j+1))
 
             ! Coefs
             t%AuW(i,j) = max(mw, 0.0)+t%mu*t%dy/t%dx
-            t%AuS(i,j) = max(ms, 0.0)+t%mu*t%dx/t%dy
             t%AuE(i,j) = max(-me, 0.0)+t%mu*t%dy/t%dx
-            t%AuN(i,j) = max(-mn, 0.0)+t%mu*t%dx/t%dy
-            t%AuP(i,j) = t%AuW(i,j)+t%AuS(i,j)+t%AuE(i,j)+t%AuN(i,j)+ms-mw+mn-ms
+            t%AuP(i,j) = t%AuW(i,j)+t%AuS(i,j)+t%AuE(i,j)+t%AuN(i,j)+me-mw+mn-ms
+
+            ! Center cells
+            if (.not.((j .eq. 1) .or. (j .eq. t%ny))) then
+                t%AuS(i,j) = max(ms, 0.0)+t%mu*t%dx/t%dy
+                t%AuN(i,j) = max(-mn, 0.0)+t%mu*t%dx/t%dy
+
+            ! Boundary cells
+            else
+                t%AuS(i,j) = max(ms, 0.0)+2.0*t%mu*t%dx/t%dy
+                t%AuN(i,j) = max(-mn, 0.0)+2.0*t%mu*t%dx/t%dy
+            end if
 
         end do
     end do
-
-    ! Apply x-momentum boundary conditions
 
     ! Iterate x-momentum
     do iter=1,10
@@ -261,5 +305,86 @@ subroutine ns_case_x_mom(t)
     end do
 
 end subroutine ns_case_x_mom
+
+
+subroutine ns_case_y_mom(t)
+    type(ns_case_t) :: t
+    integer :: iter, i, j
+    real(kind=8) :: mw, ms, me, mn
+
+    ! Calculate y-momentum coefficients
+    do i=1,t%nx
+        do j=2,t%ny
+
+            ! Mass flux terms
+            mw = 0.5*t%rho*(t%U_old(i-1,j)+t%U_old(i,j))
+            ms = 0.5*t%rho*(t%V_old(i,j-1)+t%V_old(i,j))
+            me = 0.5*t%rho*(t%U_old(i,j)+t%U_old(i+1,j))
+            mn = 0.5*t%rho*(t%V_old(i,j)+t%V_old(i,j+1))
+
+            ! Coefs
+            t%AvS(i,j) = max(ms, 0.0)+t%mu*t%dx/t%dy
+            t%AvN(i,j) = max(-mn, 0.0)+t%mu*T%dx/t%dy
+            t%AvP(i,j) = t%AvW(i,j)+t%AvS(i,j)+t%AvE(i,j)+t%AvN(i,j)+me-mw+mn-ms
+
+            ! Center cells
+            if (.not.((i .eq. 1) .or. (i .eq. t%nx))) then
+                t%AvW(i,j) = max(mw, 0.0)+t%mu*t%dy/t%dx
+                t%AvE(i,j) = max(-me, 0.0)+t%mu*t%dy/t%dx
+
+            ! Boundary cells
+            else
+                t%AvW(i,j) = max(mw, 0.0)+2.0*t%mu*t%dy/t%dx
+                t%AvE(i,j) = max(-me, 0.0)+2.0*t%mu*t%dy/t%dx
+
+            end if
+
+        end do
+    end do
+
+    ! Iterate y-momentum
+    do iter=1,10
+        do i=1,t%nx
+            do j=2,t%ny
+
+                t%V(i,j) = (1.0-t%omegaV)*t%V_old(i,j)+&
+                t%omegaV/t%AvP(i,j)*(t%AvW(i,j)*t%V(i-1,j)+t%AvS(i,j)*t%V(i,j-1)+t%AvE(i,j)*t%V(i+1,j)+t%AvN(i,j)*t%V(i,j+1)+&
+                (t%P(i,j-1)-t%P(i,j+1)*t%dx))
+
+            end do
+        end do
+    end do
+
+end subroutine ns_case_y_mom
+
+
+subroutine ns_case_pressure_corr(t)
+    type(ns_case_t) :: t
+    integer :: iter, i, j
+
+    ! Calculate coefficients
+    do i=1,t%nx
+        do j=1,t%ny
+            t%AW(i,j) = t%rho*t%dy**2/t%AuP(i,j)
+            t%AS(i,j) = t%rho*t%dx**2/t%AvP(i,j)
+            t%AE(i,j) = t%rho*t%dy**2/t%AuP(i+1,j)
+            t%AN(i,j) = t%rho*t%dx**2/t%AvP(i,j+1)
+            t%S(i,j) = t%rho*((t%U(i+1,j)-t%U(i,j))*t%dy+(t%V(i,j+1)-t%V(i,j))*t%dx)
+        end do
+    end do
+
+    t%AP = t%AW+t%AS+t%AE+t%AN
+
+    ! Iterate
+    do iter=1,1000
+        do i=1,t%nx
+            do j=1,t%ny
+                t%P_prime(i,j) = t%P_prime(i,j)+t%omegaP/t%AP(i,j)*(t%AW(i,j)*t%P_prime(i-1,j)+t%AS(i,j)*t%P_prime(i,j-1)+&
+                t%AE(i,j)*t%P_prime(i+1,j)+t%AN(i,j)*t%P_prime(i,j+1)-t%S(i,j)-t%AP(i,j)*t%P_prime(i,j))
+            end do
+        end do
+    end do
+
+end subroutine ns_case_pressure_corr
 
 end module ns_case_m
